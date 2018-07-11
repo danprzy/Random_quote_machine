fcbfbcc967f88cd97b64860827e693795389379a
Danuta Przydrozna <danprzy@gmail.com>
2018-07-11 18:55:50 +0200

    Add readme, pretiffy code

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Random quote machine
+# Random quote generator from API
+
+## Live Demo
+https://danprzy.github.io/Random_quote_machine/
+
+## Technologies
+* HTML5
+* CSS
+* SCSS
+* JavaScript
+* jQery
+* AJAX
+* Font Awesome
+
+## How does it work?
+By pressing the "New quote" button, it makes an AJAX call to a random quote generator - API Quotes on Design.
+You can tweet the current quote by clicking on the "Tweet" button.
+
--- scripts.js
+++ scripts.js
@@ -1,21 +1,22 @@
-$(document).ready(function () {
+$(document).ready(function() {
     var tweetLink = "https://twitter.com/intent/tweet?text=";
     var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
     var prefix = "https://cors-anywhere.herokuapp.com/";
 
     function getQuote() {
-  $.getJSON(prefix + quoteUrl, createTweet); // quoteUrl - link to API // createTweet - function
-  $.ajaxSetup({cache: false});
-    /* or use:
-    $.ajax({
-        dataType: "json",
-        url: quoteUrl,
-        data: null,
-        success: createTweet
-    });  */
+        $.getJSON(prefix + quoteUrl, createTweet); // quoteUrl - link to API // createTweet - function
+        $.ajaxSetup({ cache: false });
+    /* or use:  function getQuote() {
+        $.ajax({
+            dataType: "json",
+            url: quoteUrl,
+            data: null,
+            success: createTweet
+        }); } */
     }
-    function createTweet(input) {
-        var data = input[0];
+
+    function createTweet(response) {
+        var data = response[0];
 
         var quoteText = $(data.content).text().trim();
         var quoteAuthor = data.title;
@@ -29,14 +30,14 @@ $(document).ready(function () {
             getQuote();
         } else {
             var tweet = tweetLink + encodeURIComponent(tweetText);
-    $('.quote').text(quoteText);
-    $('.author').text("Author: " + quoteAuthor);
-    $('.tweet').attr('href', tweet);
+            $('.quote').text(quoteText);
+            $('.author').text("Author: " + quoteAuthor);
+            $('.tweet').attr('href', tweet);
         }
     }
-  $(document).ready(function () {
+    $(document).ready(function() {
         getQuote();
-    $('.new-quote-btn').click(function () {
+        $('.new-quote-btn').click(function() {
             getQuote();
         });
     });
